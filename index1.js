a117386365d0bf29431076d33e8ae50773b0b9ab
thisAtlas <magnus.kartvedt.p@gmail.com>
2017-02-22 13:28:13 +0100

    1.3

--- index1.js
+++ index1.js
@@ -3,18 +3,32 @@ var macAddress = "00:06:66:7D:83:8D";
 
 function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
-	document.getElementByID("forward").addEventListener("touchstart", sendForward, false);
-	document.getElementByID("forward").addEventListener("touchend", sendOff, false);
-	document.getElementByID("left").addEventListener("touchstart", sendLeft, false);
-	document.getElementByID("left").addEventListener("touchend", sendOff, false);
+	
+	document.getElementById("off").addEventListener("touchstart", sendOff, false);
+	document.getElementById("off").addEventListener("touchend", sendOff, false);
+	
+	document.getElementById("speedDown").addEventListener("touchstart", sendSpeedDown, false);
+	//document.getElementById("speedDown").addEventListener("touchend", sendOff, false);
+	
+	document.getElementById("speedUp").addEventListener("touchstart", sendSpeedUp, false);
+	//document.getElementById("speedUp").addEventListener("touchend", sendOff, false);
+	
+	document.getElementById("forward").addEventListener("touchstart", sendForward, false);
+	document.getElementById("forward").addEventListener("touchend", sendOff, false);
+	
+	document.getElementById("left").addEventListener("touchstart", sendLeft, false);
+	document.getElementById("left").addEventListener("touchend", sendOff, false);
+	
 	document.getElementById("right").addEventListener("touchstart", sendRight, false);
 	document.getElementById("right").addEventListener("touchend", sendOff, false);
-	document.getElementByID("back").addEventListener("touchstart", sendBack, false);
+	
+	document.getElementById("back").addEventListener("touchstart", sendBack, false);
 	document.getElementById("back").addEventListener("touchend", sendOff, false);
 }
 
 function sendOff() {
 	sendToArduino('s');
+	console.log("s");
 }
 function sendSpeedDown() {
 	sendToArduino('-');
